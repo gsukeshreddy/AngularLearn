ac74d609a91c1ac04d35b319c3ac4fe3c855808f
Sukesh Reddy Gangumalla <gsukeshreddy13@hotmail.com>
2025-06-23 13:06:31 -0400

    Added the Cart Page logic

--- angular-ecommerce/src/app/features/cart/pages/cart-page/cart-page.ts
+++ angular-ecommerce/src/app/features/cart/pages/cart-page/cart-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Product } from '../../../product/models/product';
+import { CartService } from '../../services/cart-service';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,4 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './cart-page.html',
   styleUrl: './cart-page.css',
 })
-export class CartPage {}
+export class CartPage implements OnInit {
+  cartItems = signal<Product[]>([]);
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.cartItems$.subscribe((data) => {
+      this.cartItems.set(data);
+    });
+  }
+
+  removeFromCart(product: Product) {
+    this.cartService.removeCartItem(product);
+  }
+
+  getCartTotal(): number {
+    return parseFloat(
+      this.cartItems()
+        .reduce((sum, item) => sum + (item.price || 0), 0)
+        .toFixed(2)
+    );
+  }
+}
